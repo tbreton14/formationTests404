d995e987fe8a5fbd43e66f7adcf6952f469051b2
Thomas BRETON <thomas.breton@livurstay.com>
2020-06-22 15:09:16 +0200

    add guzzle client

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
