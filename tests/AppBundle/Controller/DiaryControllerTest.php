a4aa33727d4d0e9bdff032748807155de00c4d31
Thomas BRETON <thomas.breton@livurstay.com>
2020-06-23 13:59:18 +0200

    modif test with crawler

--- tests/AppBundle/Controller/DiaryControllerTest.php
+++ tests/AppBundle/Controller/DiaryControllerTest.php
@@ -8,16 +8,27 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\Entity\FoodRecord;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HTTPFoundation\Response;
+use Doctrine\Persistence\ObjectManager;
 
 class DiaryControllerTest extends WebTestCase
 {
     private $client = null;
 
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
     public function setUp()
     {
         $this->client = static::createClient();
+        self::bootKernel();
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 
     public function testHomepage()
@@ -36,15 +47,81 @@ class DiaryControllerTest extends WebTestCase
 
         $form = $crawler->selectButton('Ajouter')->form([]);
 
-        $form['food[type]'] = 'Légume';
+        $form['food[type]'] = 'Fruit';
         $form['food[entitled]'] = 'Poireaux';
         $form['food[calories]'] = 80;
         $form['food[teneurProteine]'] = 10;
         $crawler = $this->client->submit($form);
 
-        //$this->client->followRedirect();
+        $this->client->followRedirect();
 
         echo $this->client->getResponse()->getContent();
+    }
+
+    public function testList()
+    {
+
+        $crawler = $this->client->request('GET', '/diary/list');
+
+        $link = $crawler->selectLink('Liste des ingrédients')->link();
+        $crawler = $this->client->click($link);
+
+        static::assertEquals(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testEditRecord()
+    {
+        $foodRecords = $this->em->getRepository(FoodRecord::class)->findBy([],['id'=> 'DESC'],1);
+
+        $crawler = $this->client->request('GET', '/diary/edit-new-record/'.$foodRecords[0]->getId());
+
+        $form = $crawler->selectButton('Modifier')->form([]);
+
+        $form['food[type]'] = 'Légume';
+        $crawler = $this->client->submit($form);
+
+        $this->client->followRedirect();
+
+        echo $this->client->getResponse()->getContent();
+    }
+
+    public function testDeleteRecord()
+    {
+        $foodRecords = $this->em->getRepository(FoodRecord::class)->findBy([],['id'=> 'DESC'],1);
+
+        $this->client->request('GET', '/diary/delete/'.$foodRecords[0]->getId());
+
+        static::assertEquals(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode()
+        );
+
+    }
+
+    public function testDeleteRecordIdNotExist()
+    {
+
+        $this->client->request('GET', '/diary/delete/0');
+
+        static::assertEquals(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode()
+        );
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
     }
 
 }
